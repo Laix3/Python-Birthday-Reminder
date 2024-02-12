f4eb7ce1560af5e2f381765fcea2a40f7390285a
agentCrafteur289 <agentcr289@gmail.com>
2024-02-12 09:52:58 +0100

    Update NotifAnniversaire_startup.py

--- NotifAnniversaire_startup.py
+++ NotifAnniversaire_startup.py
@@ -13,17 +13,12 @@ logging.basicConfig(
 
 data = openFile('data.csv')
 
+date_actuelle = datetime.date.today().strftime("%d/%m/%Y")
 
-date_actuelle = datetime.date.today().strftime("%d/%m")
-
-# Récupérer les dates d'anniversaire
-dates_anniversaire = {personne["date"]: personne["prenom"] for personne in data}
-
-# Vérifier si c'est l'anniversaire de quelqu'un aujourd'hui
-anniversaires = [prenom for date, prenom in dates_anniversaire.items() if date == date_actuelle]
-
-if anniversaires:
-    message = "C'est l'anniversaire de : \n" + "\n".join(anniversaires)
-    ctypes.windll.user32.MessageBoxW(0, message, "Anniversaires", 1)
-else:
-    ctypes.windll.user32.MessageBoxW(0, "Aucun anniversaire aujourd'hui.", "Fin", 1)
+for x in data:
+    date = x['date']
+    if date == date_actuelle:
+        ctypes.windll.user32.MessageBoxW(0, f"C'est l'anniversaire de {x['prenom']} {x['nom']} aujourd'hui", "Anniversaires", 1)
+    
+    else:
+        quit()
