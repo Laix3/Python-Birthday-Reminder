da6a9c8cdde6284afedc90df525213f17c52c515
agentCrafteur289 <agentcr289@gmail.com>
2024-02-12 09:58:55 +0100

    main

--- NotifAnniversaire_startup.py
+++ NotifAnniversaire_startup.py
@@ -13,11 +13,13 @@ logging.basicConfig(
 
 data = openFile('data.csv')
 
-date_actuelle = datetime.date.today().strftime("%d/%m/%Y")
+date_actuelle = datetime.date.today().strftime("%d/%m")
 
 for x in data:
     date = x['date']
-    if date == date_actuelle:
+    jour_mois = date.rsplit('/', 1)[0]
+    print(jour_mois)
+    if jour_mois == date_actuelle:
         ctypes.windll.user32.MessageBoxW(0, f"C'est l'anniversaire de {x['prenom']} {x['nom']} aujourd'hui", "Anniversaires", 1)
     
     else:
