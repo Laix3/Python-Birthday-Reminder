d9e7e77decfd9932cf1da25a82caf8fd709691de
agentCrafteur289 <agentcr289@gmail.com>
2024-02-12 10:00:16 +0100

    Update NotifAnniversaire_startup.py

--- NotifAnniversaire_startup.py
+++ NotifAnniversaire_startup.py
@@ -18,7 +18,7 @@ date_actuelle = datetime.date.today().strftime("%d/%m")
 for x in data:
     date = x['date']
     jour_mois = date.rsplit('/', 1)[0]
-    print(jour_mois)
+
     if jour_mois == date_actuelle:
         ctypes.windll.user32.MessageBoxW(0, f"C'est l'anniversaire de {x['prenom']} {x['nom']} aujourd'hui", "Anniversaires", 1)
     
